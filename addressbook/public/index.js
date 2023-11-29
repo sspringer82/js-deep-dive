e141da305d6771a64009fb61354f57e49cbbe988
Sebastian Springer <88656+sspringer82@users.noreply.github.com>
2023-11-29 17:23:18 +0100

    update

--- addressbook/public/index.js
+++ addressbook/public/index.js
@@ -9,6 +9,7 @@ function addDeleteButton(id, tr) {
   const button = document.createElement('button');
   button.id = id;
   button.innerText = 'löschen';
+  button.classList = 'delete';
   // button.addEventListener('click', (event) => {
   //   fetch(`http://localhost:8080/addresses/${id}`, {
   //     method: 'DELETE',
@@ -23,17 +24,42 @@ function addDeleteButton(id, tr) {
   tr.appendChild(td);
 }
 
+function addEditButton(id, tr) {
+  const td = document.createElement('td');
+  const button = document.createElement('button');
+  button.id = id;
+  button.classList = 'edit';
+  button.innerText = 'edit';
+  td.appendChild(button);
+  tr.appendChild(td);
+}
+
 function handleDelete() {
   document.querySelector('tbody').addEventListener('click', (event) => {
-    const id = event.target.id;
-    fetch(`http://localhost:8080/addresses/${id}`, {
-      method: 'DELETE',
-      headers: { 'Content-Type': 'application/json' },
-    }).then((response) => {
-      if (response.ok) {
-        event.target.parentNode.parentNode.remove();
-      }
-    });
+    if (event.target.classList[0] === 'delete') {
+      const id = event.target.id;
+      fetch(`http://localhost:8080/addresses/${id}`, {
+        method: 'DELETE',
+        headers: { 'Content-Type': 'application/json' },
+      }).then((response) => {
+        if (response.ok) {
+          event.target.parentNode.parentNode.remove();
+        }
+      });
+    }
+  });
+}
+
+function handleEdit() {
+  document.querySelector('tbody').addEventListener('click', async (event) => {
+    if (event.target.classList[0] === 'edit') {
+      const id = event.target.id;
+      const response = await fetch(`http://localhost:8080/addresses/${id}`);
+      const data = await response.json();
+      Object.entries(data).forEach((entries) => {
+        document.querySelector(`#${entries[0]}`).value = entries[1];
+      });
+    }
   });
 }
 
@@ -55,6 +81,7 @@ function createRow(address, tbody) {
   createCell(address.country, tr);
 
   addDeleteButton(address.id, tr);
+  addEditButton(address.id, tr);
 
   tbody.appendChild(tr);
 }
@@ -63,6 +90,7 @@ document.addEventListener('DOMContentLoaded', () => {
   handleForm();
 
   handleDelete();
+  handleEdit();
 
   const tbody = document.querySelector('tbody');
   fetch('http://localhost:8080/addresses')
@@ -75,6 +103,8 @@ document.addEventListener('DOMContentLoaded', () => {
 function handleForm() {
   const form = document.querySelector('form');
   form.addEventListener('submit', async (event) => {
+    const id = document.querySelector('#id').value;
+
     event.preventDefault();
 
     // daten sammeln
@@ -87,9 +117,18 @@ function handleForm() {
       country: document.querySelector('#country').value,
     };
 
+    let url = 'http://localhost:8080/addresses';
+    let method = 'POST';
+    if (id) {
+      newAddress.id = id;
+      url += '/' + id;
+      method = 'PUT';
+    }
+
     // daten zum server senden
-    const response = await fetch('http://localhost:8080/addresses', {
-      method: 'POST',
+
+    const response = await fetch(url, {
+      method,
       headers: {
         'Content-Type': 'application/json',
       },
@@ -100,7 +139,11 @@ function handleForm() {
     }
     const savedData = await response.json();
 
-    // neue Zeile einfügen
+    if (id) {
+      document
+        .querySelector('[id="' + id + '"]')
+        .parentNode.parentNode.remove();
+    }
     const tbody = document.querySelector('tbody');
     createRow(savedData, tbody);
   });
