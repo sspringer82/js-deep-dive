2d2ec8544d121a8ec0b1bc18f9721fccc400712d
Sebastian Springer <88656+sspringer82@users.noreply.github.com>
2023-11-29 16:44:36 +0100

    Create new Address

--- addressbook/public/index.js
+++ addressbook/public/index.js
@@ -37,39 +37,74 @@ function handleDelete() {
   });
 }
 
+function createRow(address, tbody) {
+  const tr = document.createElement('tr');
+  /*
+  Object.entries(address)
+    .filter((entry) => entry[0] !== 'id')
+    .forEach(([, content]) => {
+      createCell(content, tr);
+    });
+    */
+
+  createCell(address.firstname, tr);
+  createCell(address.lastname, tr);
+  createCell(address.street, tr);
+  createCell(address.city, tr);
+  createCell(address.zip, tr);
+  createCell(address.country, tr);
+
+  addDeleteButton(address.id, tr);
+
+  tbody.appendChild(tr);
+}
+
 document.addEventListener('DOMContentLoaded', () => {
+  handleForm();
+
   handleDelete();
-  const button = document.createElement('button');
-  button.innerText = 'click me';
-  button.addEventListener('click', (event) => {
-    console.log(event.target.parentNode);
-  });
-  document.body.appendChild(button);
 
   const tbody = document.querySelector('tbody');
   fetch('http://localhost:8080/addresses')
     .then((response) => response.json())
     .then((data) => {
-      data.forEach((address) => {
-        const tr = document.createElement('tr');
-        /*
-        Object.entries(address)
-          .filter((entry) => entry[0] !== 'id')
-          .forEach(([, content]) => {
-            createCell(content, tr);
-          });
-          */
-
-        createCell(address.firstname, tr);
-        createCell(address.lastname, tr);
-        createCell(address.street, tr);
-        createCell(address.city, tr);
-        createCell(address.zip, tr);
-        createCell(address.country, tr);
-
-        addDeleteButton(address.id, tr);
-
-        tbody.appendChild(tr);
-      });
+      data.forEach((address) => createRow(address, tbody));
     });
 });
+
+function handleForm() {
+  const form = document.querySelector('form');
+  form.addEventListener('submit', (event) => {
+    event.preventDefault();
+
+    // daten sammeln
+    const newAddress = {
+      firstname: document.querySelector('#firstname').value,
+      lastname: document.querySelector('#lastname').value,
+      street: document.querySelector('#street').value,
+      city: document.querySelector('#city').value,
+      zip: document.querySelector('#zip').value,
+      country: document.querySelector('#country').value,
+    };
+
+    // daten zum server senden
+    fetch('http://localhost:8080/addresses', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(newAddress),
+    })
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error('Could not save the data');
+        }
+        return response.json();
+      })
+      .then((savedData) => {
+        // neue Zeile einfügen
+        const tbody = document.querySelector('tbody');
+        createRow(savedData, tbody);
+      });
+  });
+}
