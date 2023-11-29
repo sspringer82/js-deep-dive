1e50a7d7a11745019dc585a8622c6b7c7e340c62
Sebastian Springer <88656+sspringer82@users.noreply.github.com>
2023-11-29 14:52:55 +0100

    Aufgabe 5

---
+++ Aufgaben/Aufgabe5.md
@@ -0,0 +1,6 @@
+# Aufgabe 5 - Daten anzeigen
+
+1. Lade die Daten mit der fetch-API vom Server `http://localhost:8080/addresses`
+2. für jeden Datensatz: erzeuge ein HTML-Element (z.B. div)
+3. füge die Daten als `innerText` ein
+4. hänge das neue HTML-Element mit `document.body.appendChild` in das DOM ein
--- addressbook/public/index.js
+++ addressbook/public/index.js
@@ -1,1 +1,21 @@
-console.log('Hallo Client');
+const h1 = document.querySelector('h1'); // <h1>
+const h2 = document.querySelector('.heading'); // <h1 class="heading">
+const h3 = document.querySelector('#heading'); // <h1 id="heading">
+
+const div = document.createElement('div');
+div.innerText = 'Hallo Client';
+document.body.appendChild(div);
+
+fetch('http://localhost:8080/addresses', {
+  method: 'GET',
+  headers: {
+    'Content-Type': 'application/json',
+  },
+  // body: JSON.stringify({}),
+})
+  .then((response) => {
+    return response.json();
+  })
+  .then((data) => {
+    console.log(data);
+  });
