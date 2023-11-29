b716ecbe20e89b83ed28cb2fbbc2c2f324246063
Sebastian Springer <88656+sspringer82@users.noreply.github.com>
2023-11-29 11:20:26 +0100

    Aufgabe 2 - Teil 2

--- addressbook/index.js
+++ addressbook/index.js
@@ -2,6 +2,8 @@ import express from 'express';
 
 const app = express();
 
+app.use(express.json());
+
 app.get('/', (request, response) => {
   response.send('Hallo Client');
 });
@@ -35,6 +37,19 @@ app.get('/addresses/:id', (request, response) => {
   const id = parseInt(request.params.id, 10);
   const address = data.find((item) => item.id === id);
   response.json(address);
+});
+
+app.post('/addresses', (request, response) => {
+  const id = data.length + 1;
+
+  const newAddress = {
+    ...request.body,
+    id,
+  };
+
+  data.push(newAddress);
+
+  response.json(newAddress);
 });
 
 app.listen(8080, () =>
