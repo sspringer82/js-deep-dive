091f1070173d747242355484ebebed00a3ecfe21
Sebastian Springer <88656+sspringer82@users.noreply.github.com>
2023-11-29 10:56:06 +0100

    Aufgabe 2 - Teil 1

--- addressbook/index.js
+++ addressbook/index.js
@@ -6,6 +6,37 @@ app.get('/', (request, response) => {
   response.send('Hallo Client');
 });
 
+const data = [
+  {
+    id: 1,
+    firstname: 'Klaus',
+    lastname: 'Müller',
+    street: '42 Main st',
+    city: 'New York',
+    zip: '12345',
+    country: 'USA',
+  },
+  {
+    id: 2,
+    firstname: 'Lisa',
+    lastname: 'Müller',
+    street: '42 Main st',
+    city: 'New York',
+    zip: '12345',
+    country: 'USA',
+  },
+];
+
+app.get('/addresses', (request, response) => {
+  response.json(data);
+});
+
+app.get('/addresses/:id', (request, response) => {
+  const id = parseInt(request.params.id, 10);
+  const address = data.find((item) => item.id === id);
+  response.json(address);
+});
+
 app.listen(8080, () =>
   console.log('Server is listening to http://localhost:8080')
 );
